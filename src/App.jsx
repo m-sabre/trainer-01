b40a9e5d5f82f8534101cf77c7e65f0666dc15dc
maciej <maciej.szabla@gmail.com>
2023-04-22 22:39:08 +0200

    firebasetest

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 function App() {
   const [count, setCount] = useState(0)
-  console.log(":smile:")
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyBUw35rlR6NWVCZW4F9fUZgV2K5T7vZCMc",
+    authDomain: "checklista-budowlana.firebaseapp.com",
+    projectId: "checklista-budowlana",
+    storageBucket: "checklista-budowlana.appspot.com",
+    messagingSenderId: "114082480269",
+    appId: "1:114082480269:web:df0b87bf046f6a0d80e2c4",
+    measurementId: "G-P2KKXRHMF6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
   return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
+          
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React </h1>
+      <h1>Vite + React  </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
